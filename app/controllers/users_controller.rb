42d5d6d69adb1a481e5990d7659802eb568e1821
Catrina Friday <cat.friday@gmail.com>
2020-09-25 12:03:38 -0400

    user controller and watchlist controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,41 @@
 class UsersController < ApplicationController
     
+    before_action :find_user, only: [:show, :destroy]
+    
+    def index
+        users = User.all
+        render json: users, only: [:id, :username]
+    end
 
+    def show
+        render json: @user, only: [:username, :password], methods: [posted_movies]
+    end
+
+    def create
+        user = User.new(user_params)
+    
+        if user.save
+        render json: user, only: [:username, :password], status: :created
+        else 
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+    end
+
+    def destroy
+        @user.destroy
+        render json: "User Destroyed"
+    end
+
+
+
+    private
+
+    def find_user
+        @user = User.find_by(id: params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 
 end
--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,2 +1,24 @@
 class WatchlistsController < ApplicationController
+
+    def index
+        watchlist = Watchlist.all
+        render json: watchlist
+    end
+
+    def show 
+        watchlist = Watchlist.find_by(params[:id])
+        render json: watchlist
+    end
+
+    def create 
+        watchlist = Watchlist.new(user_id: params[:user_id], movie_id: params[:movie_id])
+        watchlist.save
+        render json: watchlist
+    end
+
+   def destroy
+    watchlist = Watchlist.find_by(params[:id])
+    watchlist.destroy
+    render json: "Watchlist Has been Destroyed"
+   end
 end
