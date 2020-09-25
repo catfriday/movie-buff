6d10546440dadb4f9dc112784b2e0f5fa8c2138e
Catrina Friday <cat.friday@gmail.com>
2020-09-25 11:35:47 -0400

    added controllers and user validation in user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+    # def encode_token(payload)
+    #     JWT.encode(payload, 'mod_4_project')
+    # end
+
+    # def decoded_token(token)
+    #     JWT.decode(token, 'mod_4_project')[0]
+    #   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :watchlists
   resources :users
   resources :movies
+
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
