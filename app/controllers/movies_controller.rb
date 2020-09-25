78c1c5bdc564cb398a62d7b42564067d46e9b8fb
Catrina Friday <cat.friday@gmail.com>
2020-09-25 12:01:39 -0400

    movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,44 @@
 class MoviesController < ApplicationController
+    before_action :find_user, only: [:show, :update, :destroy]
 
+    def index
+        movies = Movie.all
+        render json: movies, except: [:created_at, :updated_at]
+    end
+
+    def show
+        render json: @movie, except: [:created_at, :updated_at]
+    end
+
+    def update
+        
+        render json: @movie, except: [:created_at, :updated_at]
+    end
+
+    def created
+        movie = Movie.new(movie_params)
+        if movie.save
+            render json: movie, except: [:created_at, :updated_at]
+        else
+            render json: 'User Not Saved'
+        end
+    end
+
+    def destroy
+        @movie.destroy
+        render json: 'Movie Destroyed'
+    end
+
+
+    private
+
+    def find_user
+        @movie = Movie.find_by(id: params[:id])
+    end
+
+    def movie_params
+        params.require(:movie).permit(:title, :genre, :review, :image, :video_link, :movie_info, :likes, :dislikes)
+    end
+    
     
 end
